713c9fac742daa15025222727e5a54a3ebac9ed9
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2016-04-25 01:33:57 +0800

    add conference class

---
+++ app/conference.rb
@@ -0,0 +1,49 @@
+class Conference
+  attr_accessor(:days, :talks, :sorted_talks, :tracks, :scheduled_tracks)
+
+  def initialize(data)
+    read_source(data)
+    refresh_days()
+    refresh_tracks()
+  end
+
+  def schedule_tracks_with_talks
+    sort_talks()
+    @tracks.each do |track|
+      track.talks = talks_in_current_track(track)
+      track.plan_talks()
+    end
+  end
+
+  private
+
+    def read_source(data)
+      @talks = data.lines.map(&:chomp).map(&:strip).reject(&:empty?).map { |line| Talk.new(line) }
+    end
+
+    def refresh_days
+      minutes = @talks.reduce(0) { |memo, talk| memo += talk.length }
+      @days = (minutes.to_f/Track.new.total_length).ceil
+    end
+
+    def refresh_tracks
+      @tracks = @days.times.map { |n| Track.new }
+    end
+
+    def sort_talks
+      @sorted_talks = @talks.sort_by { |talk| talk.length }.reverse
+    end
+
+    def talks_in_current_track(track)
+      track_total_length = track.total_length
+      @sorted_talks.reduce([]) do |memo, talk|
+        if !talk.marked && track_total_length >= talk.length
+          memo << talk
+          talk.marked = true
+          track_total_length -= talk.length
+        end
+        memo
+      end
+    end
+
+end
---
+++ spec/app/conference_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe Conference do
+  let(:conference) { build(:conference) }
+
+  it 'has properties' do
+    expect(conference).to respond_to :days
+    expect(conference).to respond_to :talks
+    expect(conference).to respond_to :tracks
+    expect(conference).to respond_to :sorted_talks
+  end
+
+  it 'initialize data source' do
+    expect(conference.days).to eq 2
+    expect(conference.talks).to be_a Array
+    expect(conference.talks.length).to eq 19
+    expect(conference.tracks).to be_a Array
+    expect(conference.tracks.length).to eq 2
+  end
+
+  it 'schedule tracks with full talks' do
+    conference.schedule_tracks_with_talks
+    expect(conference.sorted_talks.length).to eq conference.talks.length
+    expect(conference.tracks.first.talks.length).to eq 8
+    expect(conference.tracks.last.talks.length).to eq 11
+  end
+
+end
